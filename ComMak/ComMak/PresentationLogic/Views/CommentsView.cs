d6aebbdd011fab94f4f5eb3a6b27c38e5ce32ebf
lukavius <lukaszpiotrantoniak@gmail.com>
2013-11-15 23:04:14 +0100

    Dodanie widoków

---
+++ ComMak/ComMak/PresentationLogic/Views/CommentsView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComMak.PresentationLogic.Views
+{
+    public partial class CommentsView : UserControl
+    {
+        public CommentsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
